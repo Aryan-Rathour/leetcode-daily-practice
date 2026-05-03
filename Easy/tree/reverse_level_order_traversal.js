6819155a2698dc024efe1d25f5aecc2ae5a51607
Aryan rathour <aryanrathour066@gmail.com>
2026-05-04 00:02:52 +0530

    Add solution for reverse level order traversal of a binary tree

---
+++ Easy/tree/reverse_level_order_traversal.js
@@ -0,0 +1,52 @@
+// - Problem link : https://www.geeksforgeeks.org/problems/reverse-level-order-traversal/1
+// - Date : 03/04/2026
+// - Difficulty: easy
+// - Approach :Tree
+
+
+class Node
+{
+    constructor(x){
+        this.data=x;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+class Solution {
+    reverseLevelOrder(root) {
+        // code here
+        if(!root) return [];
+
+        let result = [];
+        let queue = [];
+
+        queue.push(root);
+        while(queue.length > 0){
+            let node = queue.shift();
+
+            result.push(node.data)
+
+            if(node.right) queue.push(node.right);
+            if(node.left) queue.push(node.left)
+        }
+
+        return result.reverse()
+    }
+}
+
+
+let root = new Node(1);
+let left = new Node(2);
+let right = new Node(3) ;
+
+root.left=left;
+root.right=right;
+
+let s = new Solution();
+
+console.log(s.reverseLevelOrder(root));
